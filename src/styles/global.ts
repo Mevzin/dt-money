4b86587ec7afb78533fdaaef5174409877a06746
Thiago Torres de Souza <thiagomev@gmail.com>
2021-08-18 21:56:44 -0300

    Add Modal and first styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -20,7 +20,7 @@ export const GlobalStyle = createGlobalStyle`
 
   html{
     @media(max-width: 1080px){
-      font-size: 93.75%large;
+      font-size: 93.75%;
     }
     @media(max-width: 720px){
       font-size: 87.5%;
@@ -49,4 +49,26 @@ export const GlobalStyle = createGlobalStyle`
     opacity:0.6;
     cursor:not-allowed;
   }
+
+  .react-modal-overlay{
+    background: rgba(0,0,0,0.5);
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content{
+    width: 100%;
+    max-width:576px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.25rem;
+  }
+
 `;
